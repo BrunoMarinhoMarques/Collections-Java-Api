53a6bcc9f59f40f4ee65833b2294df07fdb104d4
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-20 14:45:12 -0300

    Adicionando a lista de tarefas.

--- collections-java-api/src/List/OperacoesBasicas/ListaTarefas.java
+++ collections-java-api/src/List/OperacoesBasicas/ListaTarefas.java
@@ -14,6 +14,38 @@ public class ListaTarefas {
     }
 
     public void AdicionarTarefa(String descricao){
-        tarefaList.add
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>(null);
+        for(Tarefa t: tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricaoTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas lista = new ListaTarefas();
+
+        System.out.println("Numero total: "+ lista.obterNumeroTotalTarefas());
+        
+        lista.AdicionarTarefa("Tarefa 1");
+        lista.AdicionarTarefa("Tarefa 1");
+        lista.AdicionarTarefa("Tarefa 2");
+
+    
+
+        
     }
 }
