3d3671689904d31c8c2eb6128ddb2739e21e0584
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-27 10:48:04 -0300

    Exercicio referente a Eventos, utilizando Map.

---
+++ collections-java-api/src/List/OperacoesBasicas/Map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,59 @@
+package List.OperacoesBasicas.Map.Ordenacao;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class AgendaEventos {
+    
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos(){
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, evento);
+
+    }
+
+    //organiza os eventos a partir do TreeMap 
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    //proximos eventos que ocorreram
+    public void obterProximoEvento(){
+     
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData =  null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        if(!eventosTreeMap.isEmpty()){
+            for(Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+                if(entry.getKey().isEqual(dataAtual)||entry.getKey().isAfter(dataAtual)){
+                    proximaData = entry.getKey();
+                    proximoEvento = entry.getValue();
+                    System.out.println("O próximo evento:" + proximoEvento + " acontecerá na data "+ proximaData);
+                }
+
+            }
+        }
+    
+    }
+
+    public static void main(String[] args) {
+        
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2023, 9, 28), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, 9, 27), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Evento 3", "Atração 3");
+
+        agendaEventos.exibirAgenda();
+        agendaEventos.obterProximoEvento();
+        
+    
+    }  
+    
+}
